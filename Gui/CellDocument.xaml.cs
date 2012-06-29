a5851a5b47e6690ca162e539861e31c096cc9677
Liz <liz@lunivore.com>
2012-06-29 13:27:01 +0100

    First pass of GUI

---
+++ Gui/CellDocument.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoque.Gui
+{
+    /// <summary>
+    /// Interaction logic for CellDocument.xaml
+    /// </summary>
+    public partial class CellDocument : UserControl
+    {
+        public CellDocument()
+        {
+            InitializeComponent();
+        }
+    }
+}
